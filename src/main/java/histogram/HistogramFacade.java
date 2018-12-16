91074337e124db2516aaf7843d7763a64c4f1a04
klimson <klimson@gitlab.com>
2018-12-16 10:30:28 +0100

    Revert delete test directory

---
+++ src/main/java/histogram/HistogramFacade.java
@@ -0,0 +1,30 @@
+package histogram;
+
+import histogram.CSV.HistogramCSVGenerator;
+import histogram.factory.HistogramFactory;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class HistogramFacade {
+
+    HistogramFactory histogramFactory = new HistogramFactory();
+    HistogramCSVGenerator histogramCSVGenerator = new HistogramCSVGenerator();
+
+    public Map<Character, Long> generateHistogram(String text) {
+        //return histogram from passed text
+        return null;
+    }
+
+    public String generateHistogramCSV(String text) {
+        //return histogram CSV in String
+        return null;
+    }
+
+    public void saveHistogramToCSV(String text, String fileName) {
+        //generate histogram from passed text as argument and save him as CSV to file
+    }
+
+}
